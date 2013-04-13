2a48ca6e82ce09d992f683483b2f642adef2db5c
Arve Knudsen <arve.knudsen@gmail.com>
2013-04-13 17:08:22 +0200

    Improve tests

--- test-main.js
+++ test-main.js
@@ -1,5 +1,5 @@
 var tests = Object.keys(window.__karma__.files).filter(function (file) {
-      return /Spec\.js$/.test(file);
+  return /Spec\.js$/.test(file);
 });
 
 requirejs.config({
@@ -9,22 +9,29 @@ requirejs.config({
     paths: {
         'chai': '../node_modules/chai/chai',
         'linq2indexeddb': '../lib/Linq2IndexedDB',
-        'jquery': '../lib/jquery-1.9.1'
+        'jquery': '../lib/jquery-1.9.1',
+        'underscore': '../node_modules/underscore/underscore',
+        stacktrace: '../lib/stacktrace-0.4'
     },
 
     shim: {
         'linq2indexeddb': {
             deps: ['jquery'],
             exports: 'linq2indexedDB'
+        },
+        underscore: {
+            exports: '_'
+        },
+        stacktrace: {
+            exports: 'printStackTrace'
         }
     },
 });
 
-requirejs(tests, function (test) {
-    console.log("OK fine", tests);
+requirejs(tests, function () {
     window.__karma__.start();
 }, function (error) {
-    console.log("Error!");
+    console.log("RequireJS error!");
     console.log(error.requireType);
     console.log(error.requireModules);
 });
--- test/dbSpec.js
+++ test/dbSpec.js
@@ -1,31 +1,66 @@
 "use strict";
 
-define(["db", "linq2indexeddb", "chai"], function (db, linq2indexeddb, chai) {
-	var should = chai.should();
-	describe("db", function () {
-		var _db;
+define(["db", "linq2indexeddb", "chai", "underscore", "stacktrace"], function (db, linq2indexeddb, chai, _,
+    printStacktrace) {
+    var should = chai.should();
 
-		beforeEach(function () {
-			_db = linq2indexeddb("test", null, true);
-		});
+    describe("db", function () {
+        var _db;
 
-		it("can add objects", function (done) {
-			var refObj = {"key": "value"};
-			_db.linq.from("store").insert(refObj, "Key");
-			_db.linq.from("store").get("Key")
-			.done(function (gotObj) {
-				console.log("Done!");
-				if (gotObj === refObj) {
-					console.log("gotObj", gotObj);
-					done();
-				} else {
-					done("The loaded object doesn't equal the original one");
-				}
-			})
-			.fail(function (err) {
-				console.log("Error!");
-				done("An error occurred:", err);
-			});
-		});
-	});
+        function fail(done, reason, err) {
+            if (typeof reason === "string") {
+                reason = new Error(reason);
+            }
+            if (!reason) {
+                console.log(typeof done, typeof reason);
+                reason = new Error("There's been an error, but no reason was supplied!");
+                var st = printStacktrace({e: reason});
+                console.log(st);
+            }
+            if (typeof done !== "function") {
+                throw new Error("Was not supplied a function for 'done'!");
+            }
+            done(reason);
+        }
+
+        // Bind done as the first argument to the fail function
+        function bindFail(done, reason) {
+            if (typeof done !== "function") {
+                throw new Error("done must be a function");
+            }
+            return _.partial(fail, done, reason);
+        }
+
+        beforeEach(function (done) {
+            _db = linq2indexeddb("test", null, true);
+            _db.deleteDatabase()
+            .done(function () {
+                _db.initialize()
+                .done(done)
+                .fail(bindFail(done, "Initializing database failed"));
+            })
+            .fail(bindFail(done, "Deleting database failed"));
+        });
+
+        it("can add objects", function (done) {
+            console.log("Starting test");
+            var refObj = {"key": "value"};
+            _db.linq.from("store").insert(refObj, "Key")
+            .done(function () {
+                console.log("Added object successfully");
+                _db.linq.from("store").get("Key")
+                .done(function (gotObj) {
+                    console.log("Got object", gotObj);
+                    if (!gotObj) {
+                        return fail(done, "The result is null/undefined");
+                    }
+                    if (gotObj.key !== refObj.key) {
+                        return fail("The loaded object doesn't equal the original one", done);
+                    }
+                })
+                .fail(bindFail(done, "Getting object failed"));
+            })
+            .fail(bindFail(done, "Inserting object failed"));
+        });
+    });
 });
